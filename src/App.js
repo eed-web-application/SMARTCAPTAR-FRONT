9c606ed05c27e58adeb2547a910eca88c6a41cd1
Sean O'Callaghan <60556621+Sean-OCallaghan@users.noreply.github.com>
2023-08-29 23:33:21 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
   };
 
   return (
-    <BrowserRouter basename=“/smartcaptar“ >
+    <BrowserRouter basename={'/smartcaptar'}>
       <Routes>
         <Route
           path="/"
